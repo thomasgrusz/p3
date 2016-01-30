72909d091a7e09d65b1a4e400eb3e4a8ee36c6d0
Thomas Grusz <thomas.grusz@gmail.com>
2016-01-30 11:42:00 +0100

    feat: Improve code
    Add global control variables
    On collision event reset ALL enemies
    Concat collision function
    Fix random number function
    Change enemy y-coordinates object to array

--- js/app.js
+++ js/app.js
@@ -1,136 +1,165 @@
-// Define Enemy class and prototype methods
-var Enemy = function(name) {
-    this.name = name;
-    this.x = 0;
-    this.y = enemy_y_coords[random_number(1, 3)];
-    this.speed = random_number(100, 400);
+/*
+*
+*   Define Enemy class and corresponding prototype methods
+*
+*/
+var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
+    enemyInit(this);
 };
 
-var enemy_y_coords =
-    {
-        1: 60,
-        2: 143,
-        3: 226
-    };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {                         // eslint-disable-line
-// Check if bug left the window and if yes start again at random y-axis and random speed
-    if (this.x < 505) {
+// Update the x,y coordinates of an enemy and check for collision with player
+Enemy.prototype.update = function(dt) {
+
+// Check if enemy leaves the window and if yes, start again at random y-axis and random speed
+    if (this.x < enemyXMax) {
         this.x = this.x + (this.speed * dt);
     } else {
-        this.x = -100;
-        this.y = enemy_y_coords[random_number(1, 3)];
-        this.speed = random_number(100, 400);
+        enemyInit(this);
     }
 
-// Check if bug and player collide, if yes place player to origin and restart bug
+// Check if enemy and player collide, if yes place player to origin and reset all enemies
     collision_detect(this);
     if (collision_flag === true) {
-        player.x = 201;
-        player.y = 404;
-        this.x = - 100;
-        this.y = enemy_y_coords[random_number(1, 3)];
-        this.speed = random_number(100, 400);
+        player.update();
+        allEnemies.forEach(function(enemy) {
+            enemyInit(enemy);
+        });
         collision_flag = false;
     }
 };
 
-// Draw the enemy on the screen, required method for game
+// Render an enemy
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  // eslint-disable-line
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Create random numbers between bottom and top 
-function random_number(bottom, top) {
-    return bottom + Math.floor((Math.random() * top));
-}
 
-function collision_detect(enemy) {
-    if (enemy.y === 60 && player.y === 72) {
-        if (enemy.x > player.x) {
-            if (enemy.x < (player.x + 70)) {
-                collision_flag = true;
-            }
-        }
-        if (player.x > enemy.x) {
-            if (player.x < (enemy.x + 70)) {
-                collision_flag = true;
-            }
-        }
-    }
-    if (enemy.y === 143 && player.y === 155) {
-        if (enemy.x > player.x) {
-            if (enemy.x < (player.x + 70)) {
-                collision_flag = true;
-            }
-        }
-        if (player.x > enemy.x) {
-            if (player.x < (enemy.x + 70)) {
-                collision_flag = true;
-            }
-        }
-    }
-    if (enemy.y === 226 && player.y === 238) {
-        if (enemy.x > player.x) {
-            if (enemy.x < (player.x + 70)) {
-                collision_flag = true;
-            }
-        }
-        if (player.x > enemy.x) {
-            if (player.x < (enemy.x + 70)) {
-                collision_flag = true;
-            }
-        }
-    }
-}
-
-
-// Define Player class and prototype methods
+/*
+*
+*   Define Player class and corresponding prototype methods
+*
+*/
 var Player = function() {
-    this.x = 201;
-    this.y = 404;
+    this.x = playerXOrigin;
+    this.y = playerYOrigin;
     this.speed = 20;
     this.sprite = 'images/char-boy.png';
 };
 Player.prototype.update = function() {
+    if (collision_flag === true) {
+        player.x = playerXOrigin;
+        player.y = playerYOrigin;
+    }
 };
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  // eslint-disable-line
 };
 Player.prototype.handleInput = function(direction) {
-    if (direction === 'left' && this.x > 1) {
-        this.x = this.x - 100;
-    } else if (direction === 'right' && this.x < 401) {
-        this.x = this.x + 100;
-    } else if (direction === 'down' && this.y < 404) {
-        this.y = this.y + 83;
-    } else if (direction === 'up' && this.y > - 11) {
-        this.y = this.y - 83;
-        if (this.y < 72) {
-            this.x = 201;
-            this.y = 404;
+    if (direction === 'left' && this.x > playerLeftBorder) {
+        this.x = this.x - playerXStep;
+    } else if (direction === 'right' && this.x < playerRightBorder) {
+        this.x = this.x + playerXStep;
+    } else if (direction === 'down' && this.y < playerLowerBorder) {
+        this.y = this.y + playerYStep;
+    } else if (direction === 'up' && this.y > playerUpperBorder) {
+        this.y = this.y - playerYStep;
+        if (this.y < waterBorder) {
+            this.x = playerXOrigin;
+            this.y = playerYOrigin;
         }
     }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+/*
+*
+*   Helper Methods
+*
+*/
+
+// Initialize an enemy (x,y,speed)
+function enemyInit(enemy) {
+    enemy.x = enemyXOrigin;
+    enemy.y = enemy_y_coords[random_number(0, 2)];
+    enemy.speed = random_number(enemyMinSpeed, enemyMaxSpeed);
+}
+
+// Detect collision between enemy and player
+function collision_detect(enemy) {
+    for (var j = 0; j<= 2; j++) {
+        if (enemy.y === enemy_y_coords[j] && player.y === player_y_collision_coords[j]) {
+            if (enemy.x > player.x) {
+                if (enemy.x < (player.x + charWidth)) {
+                    collision_flag = true;
+                }
+            }
+            if (player.x > enemy.x) {
+                if (player.x < (enemy.x + charWidth)) {
+                    collision_flag = true;
+                }
+            }
+        }
+    }
+}
+
+
+// Create random numbers between 'lower' and 'upper' 
+function random_number(lower, upper) {
+    upper += 1;
+    return lower + Math.floor((Math.random() * upper));
+}
+
+
+/*
+*
+*   Define all global controll variables
+*
+*/
+var collision_flag = false;
+
 var enemyNumber = 3;
+var enemyXOrigin = -100;
+var enemyXMax = 505;
+var enemyMinSpeed = 100;
+var enemyMaxSpeed = 400;
 var allEnemies = [];
+
+var enemy_y_coords = [60, 143, 226];
+
+var playerXOrigin = 201;
+var playerYOrigin = 404;
+var playerXStep = 100;
+var playerYStep = 83;
+var playerLeftBorder = 1;
+var playerRightBorder = 401;
+var playerUpperBorder = -11;
+var playerLowerBorder = 404;
+
+var player_y_collision_coords = [72, 155, 238];
+
+var waterBorder = 72;
+var charWidth = 70;
+
+
+/*
+*   Instantiate objects by
+*   Define all global controll variables
+*   placing all enemies into an array called 'allEnemies' and
+*   placeing player object into a variable called 'player'
+*/
 for (var i = 1; i <= enemyNumber; i++) {
-    allEnemies.push(new Enemy(i));
+    allEnemies.push(new Enemy());
 }
 var player = new Player;
 
-var collision_flag = false;
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/*
+*
+*  Listen for key strokes and send keys to Player.handleInput() method.
+*
+*/
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
