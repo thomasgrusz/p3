44dc5f2e49a559169a2daf004527a857a0c77355
Thomas Grusz <thomas.grusz@gmail.com>
2016-02-08 10:37:28 +0100

    style: Change document to global variable doc in startscreen() function

--- js/engine.js
+++ js/engine.js
@@ -259,10 +259,10 @@ var Engine = (function(global) {
         ctx.fillText('press return to start!', 150, 480);
         if (player.startFirstGame === true) {
             player.startScreenDisplay = false;
-            var node = document.createElement('H2');
-            var textnode = document.createTextNode('press "space" to pause game');
+            var node = doc.createElement('H2');
+            var textnode = doc.createTextNode('press "space" to pause game');
             node.appendChild(textnode);
-            document.getElementById("body").appendChild(node);
+            doc.getElementById("body").appendChild(node);
             init();
         }
     }
